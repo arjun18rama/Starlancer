fe5995f0fff2a24e2ef5d9be1e4b3caf2d497a75
arjun18rama <81593026+arjun18rama@users.noreply.github.com>
2025-06-18 17:05:24 -0400

    Revert "Replace Interstellar branding (#35)" (#36)
    This reverts commit 94adbd06efd280b2885b7e39fd76e0429817c7f8.

--- static/assets/js/006.js
+++ static/assets/js/006.js
@@ -432,7 +432,7 @@ function importSaveData() {
         }
         alert("Your save data has been imported. Please test it out.");
         alert(
-          "If you find any issues then report it in GitHub or the Starlancer Discord.",
+          "If you find any issues then report it in GitHub or the Interstellar Discord.",
         );
       } catch (error) {
         console.error("Error parsing JSON file:", error);
